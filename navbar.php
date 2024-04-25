055dc01061640d431ca4594d72e3a4e5864bea28
Paola Alvarez <123213733+AlvarezPaola16@users.noreply.github.com>
2024-04-24 21:30:08 -0600

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,48 @@
+<section id="navegacion">
+    <nav class="navbar navbar-expand-xl navbar-light fixed-top" style="background-color: rgba(128, 0, 58, 0.822);">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#">DigitalCherry</a>
+        <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active text-white menuprincipal" aria-current="page" href="index.php">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+</svg>
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    
+                  <use xlink:href="library/icons/bootstrap-icons.svg#heart-fill" />
+                  
+                </svg> Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white menuprincipal" href="somos.php" >Somos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white menuprincipal" href="servicios.php">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white menuprincipal" href="proyectos.php">Proyectos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white menuprincipal" href="contacto.php">Contactanos</a>
+            </li>
+          </ul>
+          <span class="navbar-text">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link text-white menuprincipal" href="#">Clientes</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white menuprincipal" href="#">Ayuda</a>
+              </li>
+          </span>
+
+        </div>
+      </div>
+    </nav>
+  </section>
---
+++ principal.php
@@ -0,0 +1,203 @@
+
+<section id="Politica de calidad">
+        <div class="row ">
+        <div class="col-sm-12 col-md-12 col-lg-12 " data-aos="fade-right">
+            <img src="img/soñado1.1.png" alt="" class="img-fluid">
+        </div>
+        </section>
+            
+<section id="Nosotros">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+        <img src="img/digitalcherry1.png" class="img-fluid" alt="">
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6">
+        <div class="row mt-5 py-3">
+
+            <h2 class="text-secondary display-6 text-center text-danger font"
+            style="font-family: 'Times New Roman', Times, serif; color: purple;">
+
+            DISEÑAMOS APP INTUITIVAS
+            </h2>
+
+            <span class="text-center text-danger" style="font-family: Georgia, 'Times New Roman', Times, serif;">DIGITAL
+            CHERRY</span>
+
+            <p class="text-secondary text-center"
+            style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+            Somos una empresa que se dedica al desarrollo de softwar te invitamos a conocer mas de nosotros
+            pues aseguramos que escogernos es la mejor opcion , puedes encontar una gama de servicios para ti ¡animate
+            ya¡ </p>
+
+            <p class="text-secondary text-center"
+            style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">¿TE INTERESA SABER MAS DE
+            NOSOTROS? </p>
+            <p class="text-secondary text-center" style="font-family: Verdana, Geneva, Tahoma, sans-serif;">Te invitamos a
+            seguir conociendonos </p>
+
+        </div>
+        </div>
+    </div>
+    </section>
+    <section id="servicios">
+    <div class="container">
+        <div class="row mt-3">
+        <div class="col-sm-12 col-md-6 col-lg-3 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+                style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                Mision</h4>
+            <div class="row mt-2">
+                <p class="text-center" style="font-family: 'Times New Roman', Times, serif;">Digital Cherry, tiene la
+                misión de ser la empresa líder en el mercado de la tecnología por su competitividad y productividad de
+                nuestros servicios.
+                </p>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+                style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                Vision</h4>
+            <div class="row mt-2">
+                <p class="text-center" style="font-family: 'Times New Roman', Times, serif;">
+                Ser una empresa capaz de cumplir aquellos sueños que surgen a través de las necesidades por medio de la
+                tecnología con las herramientas necesarias para satisfacer y cubrir aquellas necesidades.
+                </p>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+                style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                Politica de Calidad </h4>
+            <div class="row mt-2">
+                <p class="text-center" style="font-family: 'Times New Roman', Times, serif;">En Digital Cherry buscamos
+                asegurar la confidencialidad, integridad y disponibilidad , nos concentramos en las necesidades y
+                expectativas de cada una de las partes interesadas.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 col-lg-3 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+                style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+                Servicios</h4>
+            <div class="row mt-2">
+                <p class="text-center">Ofrecemos diferentes servicios entre ellos, </p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
+    <section id="Politica de calidad">
+    <div class="container">
+        <div class="row mt-3">
+        <div class="col-sm-12 col-md-6 col-lg-6 mt-4" data-aos="fade-right">
+            <img src="img/creza2.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6 mt-3" data-aos="fade-left">
+            <h3 class="text-center fs-4 display-5">Politicas de <span style="color: crimson;">calidad</span></h3>
+            <div class="row">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button text-white" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
+                    style="background-color: crimson;">
+                    Mision
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    texto mision
+                    </div>
+                </div>
+                </div>
+                <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed text-white" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"
+                    style="background-color: crimson;">
+                    Vision
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <strong>texto vision
+                    </div>
+                </div>
+                </div>
+                <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed text-white " type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"
+                    style="background-color: crimson;">
+                    Valores
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <strong>texto valores
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
+    <section id="banner" class="banner mt-5">
+    <div class="container">
+        <div class="row">
+        <div class="col-sm-3 col-md-3 col-lg-3">
+            <div class="row">
+            <h3 class="text-center text-white fs-3 display-5 mt-5">Registrate</h3>
+            </div>
+
+        </div>
+        <div class="col-sm-10 col-md-10 col-lg-10">
+            <div class="row mt-3 justify-content-center text-center">
+                <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" class="needs-validation" novalidate>
+                <div class="col-sm-6 col-md-6 col-lg-6 mt-3">
+                    <input type="text" name="Nombre" id="Nombre" placeholder="Nombre" class="form-control mt-2" required>
+                    <div class="invalid-feedback"> Por favor ingresa tu nombre</div>
+                    <input type="email" name="Email" id="Email" placeholder="correo@gmail.com" class="form-control mt-2" required>
+                    <div class="invalid-feedback"> Por favor ingresa tu correo</div>
+                    <input type="password" name="Pass" id="Pass" placeholder="digita tu contraseña" class="form-control mt-2" required>
+                    <div class="invalid-feedback"> Por favor ingresa tu contraseña</div>
+                    <input type="submit" value="Registrar" name="btnRegistro" class="btn btn-sm btn-primary mt-2">
+
+                </div>
+            
+            
+            </form>
+
+            </div>
+
+
+
+
+        </div>
+        </div>
+    </div>
+        </section>
+        <script src="js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
+
+
+        
